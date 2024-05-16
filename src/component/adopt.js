20005efa22522eda21929b21164d27a1e0204c71
crystalogy <crystalramos.cmr@gmail.com>
2024-05-16 11:21:25 -0400

    Created Repository and added existing files to it

---
+++ src/component/adopt.js
@@ -0,0 +1,21 @@
+// window.addEventListener('load', function() {
+//     let header = document.querySelector('header');
+//     let footer = document.querySelector('footer');
+//     let sidebar = document.querySelector('.petSearchSideBar');
+//
+//     // Calculate the height of the sidebar
+//     let sidebarHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight;
+//
+//     // Set the height of the sidebar
+//     sidebar.style.height = sidebarHeight + 'px';
+// });
+//
+// window.addEventListener('resize', function() {
+//     let header = document.querySelector('header');
+//     let footer = document.querySelector('footer');
+//     let sidebar = document.querySelector('.petSearchSideBar');
+//
+//     // Recalculate and update the height of the sidebar when the window is resized
+//     let sidebarHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight;
+//     sidebar.style.height = sidebarHeight + 'px';
+// });
