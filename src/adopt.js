2849cd267035c87f5b8d14cfb261e27b9a311e78
crystalogy <crystalramos.cmr@gmail.com>
2024-05-18 22:21:31 -0400

    adopt page update - location bar added with drop down

---
+++ src/adopt.js
@@ -0,0 +1,5 @@
+// document.querySelector('.locationBar').style.height = '150px';
+//
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.querySelector('.locationBar').style.height = '150px';
+// });
