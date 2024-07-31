831e6733c481cfec1fc823425c6c7c11152e5479
crystalogy <crystalramos.cmr@gmail.com>
2024-07-31 19:51:58 -0400

    no message

--- springboot/src/main/webapp/pub/card.js
+++ springboot/src/main/webapp/pub/card.js
@@ -4,6 +4,9 @@ window.addEventListener('load', function() {
     for(let i = 0; i < 100; i++) {
         htmlContent += `
             <div class="card">
+            
+            
+            
     <img class="cardImg" src="` + contextPath + `/dogTestImg.jpg" alt="adoptale pet image" >
                     <div class="cardTextContainer">
                         <h4><b>Dog Doe</b></h4>
