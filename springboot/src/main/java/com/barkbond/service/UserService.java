9c72cbd32980aa171b30a169b8f9cbaf1b6cc02d
crystalogy <crystalramos.cmr@gmail.com>
2024-07-30 22:16:55 -0400

    no message

--- springboot/src/main/java/com/barkbond/service/UserService.java
+++ springboot/src/main/java/com/barkbond/service/UserService.java
@@ -6,17 +6,20 @@ import com.barkbond.form.*;
 import lombok.extern.slf4j.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.crypto.password.*;
 import org.springframework.stereotype.*;
 
 import java.util.*;
 
 @Slf4j
-@Service
+@Component
 public class UserService {
 
     @Autowired
     private UserDAO userDao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public User createUser(@NotNull CreateAccountFormBean form) {
         // there were no errors, so we can create the new user in the database
@@ -25,8 +28,11 @@ public class UserService {
         user.setEmail(form.getEmail());
 
         // we are getting a plain text password because the user entered it into the form
-        user.setPassword(form.getPassword());
-        user.setPassword(form.getFullName());
+//        user.setPassword(form.getPassword());
+        String encryptedPassword = passwordEncoder.encode(form.getPassword());
+        user.setPassword(encryptedPassword);
+        user.setFirstName(form.getFirstName());
+        user.setLastName(form.getLastName());
         user.setCreateDate(new Date());
 
         // save the user to the database
--- springboot/src/main/webapp/pub/login-signup-page.js
+++ springboot/src/main/webapp/pub/login-signup-page.js
@@ -1,18 +1,11 @@
-// Variables
-var signupButton = document.getElementById('signup-button'),
-    loginButton = document.getElementById('login-button'),
-    userForms = document.getElementById('user_options-forms')
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
 
-// Add event listener to the "Sign Up" button
-signupButton.addEventListener('click', () => {
-    userForms.classList.remove('login-click')
-    userForms.classList.add('signup-click')
-}, false)
-
-
-// Add event listener to the "Login" button
-loginButton.addEventListener('click', () => {
-    userForms.classList.remove('signup-click')
-    userForms.classList.add('login-click')
-}, false)
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
