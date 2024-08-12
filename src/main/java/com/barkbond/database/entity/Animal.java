521f38c4fbd95a99952422ad67d64f1b75f6dafb
crystalogy <crystalramos.cmr@gmail.com>
2024-08-12 13:58:34 -0400

    no message

--- src/main/java/com/barkbond/database/entity/Animal.java
+++ src/main/java/com/barkbond/database/entity/Animal.java
@@ -17,7 +17,7 @@ public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer animalID;
+    private Integer id;
 
     @Column(name = "adoption_status")
     private String status;
@@ -37,6 +37,12 @@ public class Animal {
     @Column(name = "age")
     private String age;
 
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "special_needs", columnDefinition = "TINYINT(1)")
+    private Boolean specialNeeds;
+
     @Column(name = "organization_id")
     private Integer organizationId;
 
@@ -47,8 +53,13 @@ public class Animal {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createDate;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", insertable = false, updatable = false)
+    private Organization organization;
+
     @PrePersist
     protected void onCreate() {
         createDate = new Date();
     }
+
 }
--- src/main/java/com/barkbond/database/entity/Organization.java
+++ src/main/java/com/barkbond/database/entity/Organization.java
@@ -14,6 +14,9 @@ import java.util.*;
 @Table(name = "organization")
 public class Organization {
 
+    @OneToMany(mappedBy = "organization",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private List<Animal> animals;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
